98dc2c2495106eac735b3e115eea87f1200220a9
Julien Lenne <contact.lenne@gmail.com>
2025-05-09 21:31:19 +0800

    Update comment.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,45 +1,21 @@
 return {
-  {
-    'numToStr/Comment.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        lazy = true,
+        opts = {
+            enable_autocmd = false
+        }
     },
-    config = function()
-      local opts = { noremap = true, silent = true }
-      local pre_hook_func = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
-
-      require('Comment').setup {
-        padding = true,
-        sticky = true,
-        ignore = nil,
-        toggler = {
-          line = 'gcc',
-          block = 'gbc',
-        },
-        opleader = {
-          line = 'gc',
-          block = 'gb',
-        },
-        extra = {
-          above = 'gcO',
-          below = 'gco',
-          eol = 'gcA',
-        },
-        mappings = {
-          basic = true,
-          extra = true,
-        },
-        pre_hook = pre_hook_func,
-        post_hook = nil,
-      }
-      -- C-/ for commenting, default block mode for VISUAL, you can alway use C-c for line mode in Visual!
-      vim.keymap.set('n', '<C-_>', require('Comment.api').toggle.linewise.current, opts)
-      vim.keymap.set('n', '<C-c>', require('Comment.api').toggle.linewise.current, opts)
-      vim.keymap.set('n', '<C-/>', require('Comment.api').toggle.linewise.current, opts)
-      vim.keymap.set('v', '<C-_>', "<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<cr>", opts)
-      vim.keymap.set('v', '<C-c>', "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", opts)
-      vim.keymap.set('v', '<C-/>', "<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<cr>", opts)
-    end,
-  },
+    {
+        "echasnovski/mini.comment",
+        event = "VeryLazy",
+        opts = {
+            options = {
+                custom_commentstring = function()
+                    return require("ts_context_commentstring.internal").calculate_commentstring() or
+                        vim.bo.commentstring
+                end
+            }
+        }
+    }
 }
