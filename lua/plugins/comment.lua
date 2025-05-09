ef79657879494dd8116be6bb3be625817f4ef26c
Julien Lenne <contact.lenne@gmail.com>
2025-05-09 21:38:02 +0800

    Update comment.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -13,9 +13,16 @@ return {
             options = {
                 custom_commentstring = function()
                     return require("ts_context_commentstring.internal").calculate_commentstring() or
-                        vim.bo.commentstring
+                           vim.bo.commentstring
                 end
             }
-        }
+        },
+        config = function(_, opts)
+            require('mini.comment').setup(opts)
+
+            vim.keymap.set('n', '<C-/>', 'gcc', { noremap = true, silent = true, desc = "Toggle comment line" })
+            vim.keymap.set('i', '<C-/>', '<Esc>gccA', { noremap = true, silent = true, desc = "Toggle comment line & re-enter Insert" })
+            vim.keymap.set('v', '<C-/>', 'gc', { noremap = true, silent = true, desc = "Toggle comment block" })
+        end
     }
 }
