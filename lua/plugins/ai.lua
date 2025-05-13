25fc46b6e8a86117396ac7e7a380b4d5dc719ea1
Julien Lenne <contact.lenne@gmail.com>
2025-05-13 15:44:59 +0800

    Update ai.lua

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,5 +1,9 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
+     dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+  },
   branch = "main",
   cmd = "CopilotChat",
   opts = function()
