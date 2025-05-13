e2e1f6c4bd7cc1b522ecc55cfa466363d5eddac3
Julien Lenne <contact.lenne@gmail.com>
2025-05-13 18:14:45 +0800

    desactivate lazy for copilot

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,71 +1,71 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-     dependencies = {
-      { 'github/copilot.vim' },
-      { 'nvim-lua/plenary.nvim', branch = 'master' },
+  'CopilotC-Nvim/CopilotChat.nvim',
+  dependencies = {
+    { 'github/copilot.vim', lazy = false },
+    { 'nvim-lua/plenary.nvim', branch = 'master' },
   },
   build = 'make tiktoken',
-  branch = "main",
-  cmd = "CopilotChat",
+  branch = 'main',
+  cmd = 'CopilotChat',
   opts = function()
-    local user = vim.env.USER or "User"
+    local user = vim.env.USER or 'User'
     user = user:sub(1, 1):upper() .. user:sub(2)
     return {
       auto_insert_mode = true,
-      question_header = "  " .. user .. " ",
-      answer_header = "  Copilot ",
+      question_header = '  ' .. user .. ' ',
+      answer_header = '  Copilot ',
       window = {
         width = 0.4,
       },
     }
   end,
   keys = {
-    { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
-    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+    { '<c-s>', '<CR>', ft = 'copilot-chat', desc = 'Submit Prompt', remap = true },
+    { '<leader>a', '', desc = '+ai', mode = { 'n', 'v' } },
     {
-      "<leader>aa",
+      '<leader>aa',
       function()
-        return require("CopilotChat").toggle()
+        return require('CopilotChat').toggle()
       end,
-      desc = "Toggle (CopilotChat)",
-      mode = { "n", "v" },
+      desc = 'Toggle (CopilotChat)',
+      mode = { 'n', 'v' },
     },
     {
-      "<leader>ax",
+      '<leader>ax',
       function()
-        return require("CopilotChat").reset()
+        return require('CopilotChat').reset()
       end,
-      desc = "Clear (CopilotChat)",
-      mode = { "n", "v" },
+      desc = 'Clear (CopilotChat)',
+      mode = { 'n', 'v' },
     },
     {
-      "<leader>aq",
+      '<leader>aq',
       function()
         vim.ui.input({
-          prompt = "Quick Chat: ",
+          prompt = 'Quick Chat: ',
         }, function(input)
-          if input ~= "" then
-            require("CopilotChat").ask(input)
+          if input ~= '' then
+            require('CopilotChat').ask(input)
           end
         end)
       end,
-      desc = "Quick Chat (CopilotChat)",
-      mode = { "n", "v" },
+      desc = 'Quick Chat (CopilotChat)',
+      mode = { 'n', 'v' },
     },
     {
-      "<leader>ap",
+      '<leader>ap',
       function()
-        require("CopilotChat").select_prompt()
+        require('CopilotChat').select_prompt()
       end,
-      desc = "Prompt Actions (CopilotChat)",
-      mode = { "n", "v" },
+      desc = 'Prompt Actions (CopilotChat)',
+      mode = { 'n', 'v' },
     },
   },
   config = function(_, opts)
-    local chat = require("CopilotChat")
+    local chat = require 'CopilotChat'
 
-    vim.api.nvim_create_autocmd("BufEnter", {
-      pattern = "copilot-chat",
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = 'copilot-chat',
       callback = function()
         vim.opt_local.relativenumber = false
         vim.opt_local.number = false
